00899f5ef1fbdd54c985e38e7be36e64df455447
Surendar S <surendars@Surendars-MacBook-Pro.local>
2016-10-27 19:43:40 +0530

    completed finally

--- EnumsFun/Deck.swift
+++ EnumsFun/Deck.swift
@@ -9,4 +9,34 @@
 import Foundation
 
 class Deck {
+    
+    var cards:[Card]
+    
+    init() {
+        cards = []
+        for suit in ["♠️","♦️","♥️","♣️"] {
+            
+            for rank in 2...14 {
+                
+                var newCard = Card(suit:Suit(rawValue:suit)!,rank:Rank(rawValue:rank)!)
+                cards.append(newCard)
+                
+                
+            }
+            
+        }
+        
+        self.cards = self.cards.shuffled()
+        
+    }
+    
+    
+    func split()->([Card],[Card]){
+        
+        return self.cards.split()
+        
+    }
+    
+    
+    
 }
--- EnumsFun/ViewController.swift
+++ EnumsFun/ViewController.swift
@@ -15,7 +15,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerScoreLabel: UILabel!
     @IBOutlet weak var opponentScoreLabel: UILabel!
     @IBOutlet weak var flipCardButton: UIButton!
+    
+    var ge =  GameEngine(player1:"Ramesh",player2:"Suresh")
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
 
     @IBAction func flipCard(_ sender: UIButton) {
+        
+        if !ge.gameOver() {
+            
+            let outCome = ge.playOneTurn()
+            
+            opponentCardLabel.text = outCome.2?.description
+            playerCardLabel.text = outCome.1?.description
+            winnerLabel.text = outCome.0.name
+            playerScoreLabel.text = String(describing: outCome.2?.rank.rawValue)
+            opponentScoreLabel.text = String(describing: outCome.1?.rank.rawValue)
+            
+            
+        }else {
+            
+            winnerLabel.text = "Game Over"
+            
+            
+        }
+        
+
+        
     }
 }
