1b3631af1c59ed8124dff1017eb8764c4cc6764a
ac <ac@Jaxxs-MBP.home>
2016-11-18 18:04:50 -0500

    adding Shuffle.swift

---
+++ EnumsFun/Shuffle.swift
@@ -0,0 +1,22 @@
+//
+//  Shuffle.swift
+//  ClassesMagicFun
+//
+//  Created by Michael Dippery on 7/29/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+extension MutableCollection where Index == Int {
+    mutating func shuffleInPlace() {
+        // Borrowed from: <https://codedump.io/share/OCJt0TU9IfzE/1/how-do-i-shuffle-an-array-in-swift>
+        guard count > 1 else { return }
+
+        for i in startIndex..<endIndex - 1 {
+            let j = Int(arc4random_uniform(UInt32(endIndex - i))) + i
+            guard i != j else { continue }
+            swap(&self[i], &self[j])
+        }
+    }
+}
