70719992de0ec56a703b018f6134f9692ef52d34
John Richardson <richardsonjm@gmail.com>
2016-12-02 12:53:15 -0500

    Get test to run

--- EnumsFun/Card.swift
+++ EnumsFun/Card.swift
@@ -14,7 +14,33 @@ enum Suit: String {
 
 enum Rank: Int {
     case two = 2, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace
+    
+    var stringValue: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .king:
+            return "K"
+        case .queen:
+            return "Q"
+        case .jack:
+            return "J"
+        default:
+            return "\(rawValue)"
+        }
+    }
 }
 
 class Card {
+    
+    init(rank: Rank, suit: Suit) {
+        self.rank = rank
+        self.suit = suit
+    }
+    
+    let suit: Suit
+    let rank: Rank
+    var description: String {
+       return "\(rank.stringValue)\(suit.rawValue)"
+    }
 }
--- EnumsFun/Player.swift
+++ EnumsFun/Player.swift
@@ -9,4 +9,27 @@
 import Foundation
 
 class Player {
+    var name: String
+    var hand: Hand
+    
+    init(name: String, hand: Hand) {
+        self.name = name
+        self.hand = hand
+    }
+    
+    func hasCards() -> Bool {
+        return hand.cards.count > 0
+    }
+    
+    func flip() -> Card? {
+        return hand.flip()
+    }
+    
+    func give(card: Card) {
+        hand.cards.append(card)
+    }
+    
+    func lose(card: Card) {
+        hand.lose(card: card)
+    }
 }
