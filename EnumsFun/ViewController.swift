4c9238153f0279b2eb097b14e2513559fa23682b
Marc Aupont <marc@digimarktech.com>
2017-02-07 14:28:07 -0500

    Completed EnumsFun Lab with all test working

--- EnumsFun/ViewController.swift
+++ EnumsFun/ViewController.swift
@@ -15,7 +15,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerScoreLabel: UILabel!
     @IBOutlet weak var opponentScoreLabel: UILabel!
     @IBOutlet weak var flipCardButton: UIButton!
+    
+    let warGame = GameEngine(player1: "Johnny", player2: "James")
+    
+    var johnny: Player {
+        
+        return warGame.player1
+    }
+    
+    var james: Player {
+        
+        return warGame.player2
+    }
 
     @IBAction func flipCard(_ sender: UIButton) {
+        
+        let (winner, johnnyCard, jamesCard) = warGame.playOneTurn()
+        
+        if let johnnyCard = johnnyCard {
+            
+            playerCardLabel.text = johnnyCard.description
+            
+        }
+        
+        if let jamesCard = jamesCard {
+            
+            opponentCardLabel.text = jamesCard.description
+            
+        }
+        
+        winnerLabel.text = "\(winner.name) has won!"
+        
+        playerScoreLabel.text = "\(johnny.name) hand size: \(johnny.hand.size)"
+        
+        opponentScoreLabel.text = "\(james.name) hand size: \(james.hand.size)"
+        
+        if warGame.gameOver() {
+            
+            flipCardButton.isHidden = true
+        }
     }
 }
