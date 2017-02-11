d50668efb890842bf70a4d235b8b7baf84885afc
Terrence <Terrence@Terrences-iMac.local>
2017-02-11 15:04:16 -0500

    WAR

--- EnumsFun/Card.swift
+++ EnumsFun/Card.swift
@@ -10,16 +10,54 @@ import Foundation
 
 enum Suit: String {
     
-
+    static let suits:[Suit] = [.spades, .diamonds, .hearts, .clubs]
+    
+    case spades = "♠️", diamonds = "♦️", hearts = "♥️", clubs = "♣️"
 }
 
 enum Rank: Int {
     
- 
+    static let ranks: [Rank] = [.two, .three, .four, .five, .six, .seven, .eight, .nine, .ten, .jack, .queen, .king, .ace]
+    
+    case two = 2, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace
+    
+    var stringValue:String {
+        switch self {
+        case .jack:
+            return "J"
+        case .queen:
+            return "Q"
+        case .king:
+            return "K"
+        case .ace:
+            return "A"
+        default:
+            return "\(rawValue)"
+        }
+    }
 }
 
-class Card {
+class Card: Equatable {
     
-
+    var suit: Suit
+    var rank: Rank
+    var description: String{
+        return rank.stringValue + suit.rawValue
+    }
     
+    init (suit:Suit, rank:Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
+    
+    static func == (lhs:Card, rhs:Card) -> Bool {
+        return lhs.description == rhs.description
+    }
 }
+
+
+//Add a String property to Rank called stringValue. This should return a string representing the value of the card: the numbered value for numbered cards, or "J", "Q", "K", or "A" for face cards.
+//Add a property called suit of type Suit to the Card class.
+//Add a property called rank of type Rank to the Card class.
+//Add a computed property called description of type String to Card. It should return a string representing the suit and rank of the card, in the format "<rank><suit>".
+//Create an initializer for Card that accepts both a rank and a suit and sets the appropriate properties. These parameters should be called rank and suit.
--- EnumsFun/GameEngine.swift
+++ EnumsFun/GameEngine.swift
@@ -9,10 +9,53 @@
 import Foundation
 
 class GameEngine {
+    let player1:Player
+    let player2:Player
+    let deck = Deck()
     
+    init(player1:String, player2:String){
+        let splitDeck = deck.split()
+        let player1Hand = Hand(cards: splitDeck.0)
+        let player2Hand = Hand(cards: splitDeck.1)
+        self.player1 = Player(name: player1, hand: player1Hand)
+        self.player2 = Player(name: player2, hand: player2Hand)
+    }
 
+    func playOneTurn() -> (Player, Card?, Card?) {
+        guard player1.hasCards() else {return (player2, nil, nil)}
+        guard player1.hasCards() else {return (player1, nil, nil)}
+        
+        let player1Card = player1.flip()!
+        let player2Card = player2.flip()!
+            if player1Card.rank.rawValue > player2Card.rank.rawValue {
+                award(cards: [player1Card, player2Card], to: player1)
+                return (player1, player1Card, player2Card)
+        } else {
+            award(cards: [player1Card, player2Card], to: player2)
+            return (player2, player1Card, player2Card)
+        }
     
+    }
     
+    func award(cards:[Card], to player:Player){
+        for card in cards {
+            player.give(card: card)
+        }
+        
+    }
+    
+    func gameOver() -> Bool {
+        return !player1.hasCards()
+        || !player2.hasCards()
+    }
 }
 
+//Next, open up GameEngine.swift. There is a GameEngine class defined in this file that implements the logic for a game of War.
+//Add a property called player1 of type Player to GameEngine.
+//Add a property called player2 of type Player.
+//Add a property called deck of type Deck.
+//Create an initializer for GameEngine that takes two Player parameters called player1 and player2, both of type String, representing the names of the game's players. This initializer should create a deck of cards and split it in half, then create two Player instances and assign them to the appropriate property.
+//Implement a method called playOneTurn() that returns a three-element tuple of type (Player, Card?, Card?), representing the winning player of this turn, the card that player1 flipped over, and the card that player2 flipped over. This method should flip cards for each player. Remember, the player with the highest card wins!
+//Implement a method called award(cards:to:). This methods first parameter is called cards of type [Card]. The second parameter should be called player of type Player. It should give the player all of the cards. (Hint: Don't forget about the give(card:) method you implemented on the Player class.)
+//Implement a method called gameOver() that returns true when the game is over. (The game is over when either player has run out of cards.)
 
