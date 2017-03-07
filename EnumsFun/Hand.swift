d2d751d897c6737586229eb583d6712c6535b4d2
Stephen Harrington <stephenharrington04@yahoo.com>
2017-03-07 16:05:31 -0600

    Completed Problems

--- EnumsFun/Hand.swift
+++ EnumsFun/Hand.swift
@@ -9,7 +9,33 @@
 import Foundation
 
 class Hand {
-    
+    var cards: [Card]
+    var size: Int {
+        return cards.count
+    }
 
     
+    init(cards: [Card]){
+        self.cards = cards
+    }
+    
+    func flip() -> Card? {
+        if cards.isEmpty {
+            return nil
+        } else {
+            return cards.removeLast()
+        }
+    }
+    
+    func give(card: Card) {
+        cards.insert(card, at: 0)
+    }
+    
+    func lose(card: Card) {
+        for (index, value) in cards.enumerated(){
+            if card.description == value.description {
+                cards.remove(at: index)
+            }
+        }
+    }
 }
--- EnumsFun/ViewController.swift
+++ EnumsFun/ViewController.swift
@@ -16,10 +16,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var opponentScoreLabel: UILabel!
     @IBOutlet weak var flipCardButton: UIButton!
     
-
+let warGame = GameEngine(player1: "You", player2: "Opponent")
+    var you: Player {
+        return warGame.player1
+    }
+    var opponent: Player {
+        return warGame.player2
+    }
+    
+    
+    
     
     @IBAction func flipCard(_ sender: UIButton) {
-     
+        let (winner, youCard, opponentCard) = warGame.playOneTurn()
+        
+        if let youCard = youCard{
+            playerCardLabel.text = youCard.description
+        }
+        if let opponentCard = opponentCard{
+            opponentCardLabel.text = opponentCard.description
+        }
+        winnerLabel.text = winner.name
+        
+        playerScoreLabel.text = String(you.hand.size)
+        opponentScoreLabel.text = String(opponent.hand.size)
+        
+        if warGame.gameOver() {
+            flipCardButton.isHidden = true
+        }
     }
     
    
