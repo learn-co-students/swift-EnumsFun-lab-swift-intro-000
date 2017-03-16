9b5c7551241f2615bc49ea72d6107041b43c84f5
Brendt Bly <brendtbly@Brendts-MacBook-Pro.local>
2017-03-16 07:49:38 -0500

    Completed problems

--- EnumsFun/Hand.swift
+++ EnumsFun/Hand.swift
@@ -9,7 +9,28 @@
 import Foundation
 
 class Hand {
+    var cards: [Card] = []
+    var size: Int {
+        return cards.count
+    }
+    init(cards: [Card]) {
+        self.cards = cards
+    }
+    func flip() -> Card? {
+        let card = cards[0]
+        guard !cards.isEmpty else {return nil}
+        cards.removeLast()
+        return card
+    }
     
-
+    func give(card: Card) {
+        cards.append(card)
+    }
+    
+    func lose(card: Card) {
+        if let index = cards.index(of: card) {
+            cards.remove(at: index)
+        }
+    }
     
 }
--- EnumsFun/Player.swift
+++ EnumsFun/Player.swift
@@ -9,7 +9,31 @@
 import Foundation
 
 class Player {
+    let name: String
+    var hand: Hand
     
+    init(name: String, hand: Hand) {
+        self.name = name
+        self.hand = hand
+    }
+    func hasCards() -> Bool {
+        guard !hand.cards.isEmpty else {return false}
+        return true
+    }
+    
+    func flip() -> Card? {
+        return hand.flip()
+    }
+    
+    func give(card: Card) {
+        return hand.give(card: card)
+    }
+    
+    func lose(card: Card) {
+        return hand.lose(card: card)
+    }
+    
+    //Create a function called hasCards() that returns a Bool indicating if the player has cards left in their hand or not.
  
 }
 
