4a551e1aaa8d80235fc5974abae0784c7602ed39
g3kenny <g3_kenny@hotmail.com>
2017-03-25 18:58:28 +0800

    Commit enums lab

--- EnumsFun/Deck.swift
+++ EnumsFun/Deck.swift
@@ -9,8 +9,24 @@
 import Foundation
 
 class Deck {
+    var cards: [Card]
     
-  
+    init() {
+        cards = []
+        for suit in ["♠️", "♣️", "♥️", "♦️"] {
+            for rank in 2...14 {
+                let newrank = Rank(rawValue: rank)!
+                let newsuit = Suit(rawValue: suit)!
+                let newCard = Card.init(rank: newrank, suit: newsuit)
+                cards.append(newCard)
+            }
+        }
+        cards.shuffle()
+    }
+    
+    func split() -> ([Card],[Card]) {
+        return cards.split()
+    }
 }
 
 
