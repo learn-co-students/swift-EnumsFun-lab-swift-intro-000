419d9bb68a6084d85eb7044f31a0a24758e43114
one-for-all <jianan.li@yahoo.com>
2017-05-13 19:00:50 -0700

    completed enums lab git add .

--- EnumsFun/Deck.swift
+++ EnumsFun/Deck.swift
@@ -7,9 +7,26 @@
 //
 
 import Foundation
-
+//DECK.SWIFT
+//Next, open up Deck.swift. You'll find a class in this file called Deck that represents a deck of cards.
+//Add a property called cards of type [Card] to Deck.
+//Create a new initializer than creates a deck of playing cards. This should be an array of 52 Card instances of the appropriate rank and suit. (Hint: You can use a couple of for loops to do this -- remember how to create Rank and Suit enums from raw values!) Assign this to the cards property. Make sure the deck is shuffled! (Hint: You can call shuffle() on an Array to shuffle the contents. This method was provided to you.)
+//Add a method called split(). This method should split the set of the cards in half and return a two-element tuple consisting of each half of the deck (i.e., the return type is ([Card], [Card]). (Hint: An Array method called split() has been written for you in the Array.swift file.)
 class Deck {
+    var cards: [Card]
     
+    init() {
+        cards = []
+        for suit in Suit.suits {
+            for rank in Rank.ranks {
+                cards.append(Card(rank: rank , suit: suit))
+            }
+        }
+        cards.shuffle()
+    }
+    func split() -> ([Card], [Card]) {
+        return cards.split()
+    }
   
 }
 
