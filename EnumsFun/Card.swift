495bd74d174981c6ff2f49a18102329389de001c
Mitchell Schuster <mgschuster11@gmail.com>
2017-07-19 16:22:15 +0300

    finished

--- EnumsFun/Card.swift
+++ EnumsFun/Card.swift
@@ -10,16 +10,32 @@ import Foundation
 
 enum Suit: String {
     
+    case heart = "♥️"
+    case diamond = "♦️"
+    case spade = "♠️"
+    case club = "♣️"
 
 }
 
 enum Rank: Int {
     
+    case two = 2, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace
  
 }
 
 class Card {
     
+    var suit: Suit
+    var rank: Rank
+    
+    var description: String {
+        return "\(rank)\(suit)"
+    }
+    
+    init(rank: Rank, suit: Suit) {
+        self.suit = suit
+        self.rank = rank
+    }
 
     
 }
--- EnumsFun/Deck.swift
+++ EnumsFun/Deck.swift
@@ -10,7 +10,27 @@ import Foundation
 
 class Deck {
     
-  
+    var cards: [Card]
+    
+    init(cards: [Card]) {
+        let ranks = [Rank.ace, Rank.eight, Rank.five, Rank.four, Rank.jack, Rank.king, Rank.nine, Rank.queen, Rank.seven, Rank.six, Rank.ten, Rank.three, Rank.two]
+        let suits = [Suit.club, Suit.diamond, Suit.heart, Suit.spade]
+        
+        var deck = [Card]()
+        
+        for suit in suits {
+            for rank in ranks {
+                deck.append(Card(rank: rank, suit: suit))
+            }
+        }
+
+        self.cards = deck.shuffled()
+    }
+    
+    func split() -> ([Card], [Card]) {
+        return cards.split()
+    }
+    
 }
 
 
