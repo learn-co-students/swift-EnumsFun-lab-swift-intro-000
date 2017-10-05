6c081a5c0d73756b1f008144d40a28f838bb3fa3
stephanos <stephanos@goldmail.etsu.edu>
2017-10-05 13:47:11 -0700

    Complete, Tests throw Execptions

--- EnumsFun/Hand.swift
+++ EnumsFun/Hand.swift
@@ -10,6 +10,29 @@ import Foundation
 
 class Hand {
     
-
+    var cards : [Card];
+    var size : Int {
+        return cards.count;
+    }
     
+    init(cards : [Card]) {
+        self.cards = cards;
+    }
+    
+    func flip() -> Card? {
+        if(size == 0) {
+            return nil
+        } else {
+            return cards.remove(at: 0);
+        }
+    }
+    
+    func give(card : Card) {
+        cards.append(card);
+    }
+    
+    func lose(card : Card) {
+       
+        cards.remove(at: cards.index(where: { $0.description == card.description })!)
+    }
 }
--- EnumsFun/ViewController.swift
+++ EnumsFun/ViewController.swift
@@ -17,9 +17,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var flipCardButton: UIButton!
     
 
+    var gameEngine = GameEngine(player1: "Koi", player2: "Emma");
+
     
     @IBAction func flipCard(_ sender: UIButton) {
-     
+        let results = gameEngine.playOneTurn();
+        
+        opponentCardLabel.text = results.2?.description;
+        playerCardLabel.text = results.1?.description;
+        winnerLabel.text = results.0.name;
+        if(results.0.name == "Koi") {
+            let score = Int(playerScoreLabel.text!);
+            playerScoreLabel.text = String(score! + 1);
+        } else {
+            let score = Int(opponentScoreLabel.text!);
+            opponentScoreLabel.text = String(score! + 1);
+        }
     }
     
    
