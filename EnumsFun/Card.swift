23762158048a8de2692b3bd271df00d543c2aeb9
Cynthia Nikolai <cnikolai@earthlink.net>
2018-01-12 11:47:36 -0500

    first commit

--- EnumsFun/Card.swift
+++ EnumsFun/Card.swift
@@ -14,12 +14,32 @@ enum Suit: String {
 }
 
 enum Rank: Int {
-    
- 
+    case 2,3,4,5,6,7,8,9,10,"J","Q","K","A"
+    var stringValue: String {
+        switch self {
+            case "2","3","4","5","6","7","8","9","10":
+                return String(self)
+            case "J":
+                return 11
+            case "Q":
+                return 12
+            case "K":
+                return 13
+            case "A":
+                return 14
+        }
+    }
 }
 
 class Card {
+    var suit: String
+    var rank: Int
+    var description: String {
+        return "\(rank)\(suit)"
+    }
     
-
-    
+    init(rank: Int, suit: String) {
+        self.rank = rank
+        self.suit = suit
+    }
 }
