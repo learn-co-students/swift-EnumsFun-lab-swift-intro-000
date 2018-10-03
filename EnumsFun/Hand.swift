9695d05a9b4869b5156b5c8096b8e5e992e43b12
mjanoudy <muaawiaj1991@hotmail.com>
2018-10-03 16:28:36 +0300

    finished enums fun war lab

--- EnumsFun/Hand.swift
+++ EnumsFun/Hand.swift
@@ -9,7 +9,29 @@
 import Foundation
 
 class Hand {
+    var cards: [Card]
+    var size: Int {
+        return cards.count
+    }
+    init (cards: [Card]) {
+        self.cards = cards
+    }
     
+    func flip() -> Card? {
+        if !cards.isEmpty {
+            var topCard: Card = cards[0]
+            cards.remove(at: 0)
+            return topCard
+        } else {
+            return nil
+        }
+    }
 
+   func give(card: Card) {
+        cards.append(card)
+    }
     
+    func lose() {
+        cards.dropFirst()
+    }
 }
