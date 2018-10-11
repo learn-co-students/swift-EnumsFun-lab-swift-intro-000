e5bcb2749fa7b30cb9ce115d7398c4ab80d8c97d
bodeacorina <corina.bodea@yahoo.com>
2018-10-11 10:37:37 +0300

    Add EnumsFun solution

--- EnumsFun/Card.swift
+++ EnumsFun/Card.swift
@@ -9,17 +9,40 @@
 import Foundation
 
 enum Suit: String {
-    
-
+    static let suits: [Suit] = [.spades, .hearts, .clubs, .diamonds]
+    case spades = "♠️", hearts = "♥️", clubs = "♣️", diamonds = "♦️"
 }
 
 enum Rank: Int {
+    static let ranks: [Rank] = [.ace, .two, .three, .four, .five, .six, .seven, .eight, .nine, .ten, .jack, .queen, .king]
+
+    case two = 2, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace
     
- 
+    var stringValue : String {
+        switch self {
+        case .ace:
+            return "A"
+        case .king:
+            return "K"
+        case .queen:
+            return "Q"
+        case .jack:
+            return "J"
+        default:
+            return "\(rawValue)"
+        }
+    }
 }
 
 class Card {
+    let suit : Suit
+    let rank : Rank
+    var description: String  {
+        return "\(rank)\(suit)"
+    }
     
-
-    
+    init(suit: Suit, rank: Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
 }
